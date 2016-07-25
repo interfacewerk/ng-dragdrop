d81d04449296e3303d9cc51d19d828c24c6b7bf0
Gabriel Brandersky <brandersky@interfacewerk>
2016-07-25 23:12:52 +0200

    allow dragstart and dragend callbacks

--- dist/ng1/ng-dragdrop.js
+++ dist/ng1/ng-dragdrop.js
@@ -1,78 +1,110 @@
-(function() {
-
-
-  var droppableOverClass = 'ng-droppable-over';
-  function makeDraggable(element, data) {
+var droppableOverClass = 'ng-droppable-over';
+function makeDraggable(element, data, onDragstart, onDragend) {
     element.setAttribute('draggable', 'true');
-    var dragstart = function(dragEvent) {
-      dragEvent.dataTransfer.effectAllowed = 'move';
-      dragEvent.dataTransfer.setData('text/json', JSON.stringify(data()));
+    var dragstart = function (dragEvent) {
+        dragEvent.dataTransfer.effectAllowed = 'move';
+        dragEvent.dataTransfer.setData('text/json', JSON.stringify(data()));
+        onDragstart(data());
+    };
+    var dragend = function () {
+        onDragend(data());
     };
     element.addEventListener('dragstart', dragstart);
-    return function() { element.removeEventListener('dragstart', dragstart); };
-  }
-  function makeDroppable(element, onDrop) {
-    var dragover = function(dragEvent) {
-      dragEvent.preventDefault();
-      dragEvent.dataTransfer.dropEffect = 'move';
+    element.addEventListener('dragend', dragend);
+    return function () {
+        element.removeEventListener('dragstart', dragstart);
+        element.removeEventListener('dragend', dragend);
     };
-    var dragenter = function() { element.classList.add(droppableOverClass); };
-    var dragleaveAndend = function() {
-      element.classList.remove(droppableOverClass);
+}
+function makeDroppable(element, onDrop) {
+    var dragover = function (dragEvent) {
+        dragEvent.preventDefault();
+        dragEvent.dataTransfer.dropEffect = 'move';
     };
-    var drop = function(dragEvent) {
-      dragEvent.preventDefault();
-      var data = JSON.parse(dragEvent.dataTransfer.getData('text/json'));
-      element.classList.remove(droppableOverClass);
-      onDrop(data);
+    var dragenter = function () {
+        element.classList.add(droppableOverClass);
+    };
+    var dragleaveAndend = function () {
+        element.classList.remove(droppableOverClass);
+    };
+    var drop = function (dragEvent) {
+        dragEvent.preventDefault();
+        var data = JSON.parse(dragEvent.dataTransfer.getData('text/json'));
+        element.classList.remove(droppableOverClass);
+        onDrop(data);
     };
     element.addEventListener('drop', drop);
     element.addEventListener('dragover', dragover);
     element.addEventListener('dragleave', dragleaveAndend);
-    element.addEventListener('dragend', dragleaveAndend);
     element.addEventListener('dragenter', dragenter);
-    return function() {
-      element.removeEventListener('drop', drop);
-      element.removeEventListener('dragover', dragover);
-      element.removeEventListener('dragleave', dragleaveAndend);
-      element.removeEventListener('dragend', dragleaveAndend);
-      element.removeEventListener('dragenter', dragenter);
+    return function () {
+        element.removeEventListener('drop', drop);
+        element.removeEventListener('dragover', dragover);
+        element.removeEventListener('dragleave', dragleaveAndend);
+        element.removeEventListener('dragenter', dragenter);
     };
-  }
-  angular.module('ng-dragdrop', [])
-      .directive(
-          'ngDroppable',
-          function() {
-            return {restrict: 'EA', scope: true, controller: NGDroppable};
-          })
-      .directive('ngDraggable', function() {
-        return {restrict: 'EA', scope: true, controller: NGDraggable};
-      });
-  var NGDraggable = (function() {
+}
+angular.module('ng-dragdrop', [])
+    .directive('ngDroppable', function () {
+    return {
+        restrict: 'EA',
+        scope: true,
+        controller: NGDroppable
+    };
+})
+    .directive('ngDraggable', function () {
+    return {
+        restrict: 'EA',
+        scope: true,
+        controller: NGDraggable
+    };
+});
+var NGDraggable = (function () {
     function NGDraggable($element, $scope, $attrs, $parse) {
-      var parsed = $parse($attrs['ngDraggable']);
-      var handler =
-          makeDraggable($element[0], function() { return parsed($scope); });
-      $scope.$on('$destroy', handler);
+        var parsed = $parse($attrs['ngDraggable']);
+        var ondragstart = function (data) {
+            if ($attrs['ngDraggableDragstart']) {
+                $scope.$eval($attrs['ngDraggableDragstart'], { $data: data });
+            }
+        };
+        var ondragend = function () {
+            if ($attrs['ngDraggableDragend']) {
+                $scope.$eval($attrs['ngDraggableDragend']);
+            }
+        };
+        var handler = makeDraggable($element[0], function () { return parsed($scope); }, ondragstart, ondragend);
+        $scope.$on('$destroy', handler);
     }
-    NGDraggable.$inject = ['$element', '$scope', '$attrs', '$parse'];
+    NGDraggable.$inject = [
+        '$element',
+        '$scope',
+        '$attrs',
+        '$parse'
+    ];
     return NGDraggable;
-  }());
-  var NGDroppable = (function() {
+}());
+var NGDroppable = (function () {
     function NGDroppable($element, $scope, $attrs, $parse) {
-      var callbacks = $parse($attrs['ngDroppable'])($scope);
-      $scope.$on('$destroy', makeDroppable($element[0], function(data) {
-                   for (var key in data) {
-                     if (callbacks[key]) {
-                       $scope.$eval(callbacks[key], {$data: data[key]});
-                     }
-                   }
-                   $scope.$apply();
-                 }));
+        var callbacks = $parse($attrs['ngDroppable'])($scope);
+        var onDrop = function (data) {
+            for (var key in data) {
+                if (callbacks[key]) {
+                    $scope.$eval(callbacks[key], {
+                        $data: data[key]
+                    });
+                }
+            }
+            $scope.$apply();
+        };
+        $scope.$on('$destroy', makeDroppable($element[0], onDrop));
     }
-    NGDroppable.$inject = ['$element', '$scope', '$attrs', '$parse'];
+    NGDroppable.$inject = [
+        '$element',
+        '$scope',
+        '$attrs',
+        '$parse'
+    ];
     return NGDroppable;
-  }());
+}());
 
-
-})();
+//# sourceMappingURL=tree-list.js.map
